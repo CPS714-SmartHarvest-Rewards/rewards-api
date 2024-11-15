455a0ef6cf7c6477d10dbab1963e66d10289698e
Sami Siddiqui <suzaman00@gmail.com>
2024-11-14 22:00:59 -0500

    Update urls.py

--- loyalty/urls.py
+++ loyalty/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('offers/', views.list_offers, name='list_offers'),
     path('create-reward/', views.create_reward, name='create_reward'),
     path('total-points/', views.total_points_earned, name='total_points_earned'),
+    path('bonus-points/', views.add_bonus_points, name='add_bonus_points'),  # Add bonus points for events
+    path('admin/add-reward/', views.admin_add_reward, name='admin_add_reward'),  # Admin endpoint to add reward
 ]
