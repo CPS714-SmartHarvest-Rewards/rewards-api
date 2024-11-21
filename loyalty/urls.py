4e0f948c5b6cc695354d76ab2b3c27cef388f5ab
Sami Siddiqui <suzaman00@gmail.com>
2024-11-20 23:24:21 -0500

    Update urls.py

--- loyalty/urls.py
+++ loyalty/urls.py
@@ -1,11 +1,11 @@
-# loyalty/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.loyalty_home, name='loyalty_home'),  # Default route for /loyalty/
     path('offers/', views.list_offers, name='list_offers'),
     path('create-reward/', views.create_reward, name='create_reward'),
     path('total-points/', views.total_points_earned, name='total_points_earned'),
-    path('bonus-points/', views.add_bonus_points, name='add_bonus_points'),  # Add bonus points for events
-    path('admin/add-reward/', views.admin_add_reward, name='admin_add_reward'),  # Admin endpoint to add reward
+    path('bonus-points/', views.add_bonus_points, name='add_bonus_points'),
+    path('admin/add-reward/', views.admin_add_reward, name='admin_add_reward'),
 ]
